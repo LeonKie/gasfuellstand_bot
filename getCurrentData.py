eb9be7db7b3518129ffaa730efc9b1ae4e6bd15b
schuenemann <til.schuenemann@mailbox.org>
2022-04-02 12:26:32 +0200

    parse json response from api to df

--- getCurrentData.py
+++ getCurrentData.py
@@ -1,34 +1,55 @@
-
-import numpy as np    
+ 
 import pandas as pd
-from bs4 import BeautifulSoup
 
+import datetime
+import json
+import requests
 
+def get_gas_data(api_key : str, date_start : str, date_end : str) -> pd.DataFrame:
+    """
+    Parameters:
+    api_key : str
+        api_key for AGSI+
+    date_start : str
+        date in YYYY-MM-DD format 
+    date_end : str
+        date in YYYY-MM-DD format
 
-def convert_to_xlsx():
-    
-    with open('Germany.xls') as xml_file:
-        soup = BeautifulSoup(xml_file.read(), 'xml')
-        writer = pd.ExcelWriter('Germnay.xlsx')
-        for sheet in soup.findAll('Worksheet'):
-            sheet_as_list = []
-            for row in sheet.findAll('Row'):
-                sheet_as_list.append([cell.Data.text if cell.Data else '' for cell in row.findAll('Cell')])
-            pd.DataFrame(sheet_as_list).to_excel(writer, sheet_name=sheet.attrs['ss:Name'], index=False, header=False)
+    Returns:
+        Pandas dataframe
 
-        writer.save()
+    """
 
+    url = f"https://agsi.gie.eu/api/data/de?from={date_start}&till={date_end}"
+    headers = {"x-key": api_key}
+    content = requests.request("GET",url, headers=headers)
+    gas_data_json = json.loads(content.content)
+
+    df = pd.DataFrame.from_dict(gas_data_json)
+    dict_columns_type = {'status': str,
+                    'gasDayStartedOn': str,
+                    'gasInStorage': float,
+                    'full': float,
+                    'trend': float,
+                    'injection': float,
+                    'injectionCapacity': float,
+                    'withdrawal': float,
+                    'withdrawalCapacity': float,
+                    'workingGasVolume': float,
+                    'info': str
+                }
+    return df.astype(dict_columns_type)
 
 def get_tweet_from_df(df):
 
-    fuellstand = float(df.iloc[0,2].replace(",","."))
+    fuellstand = df.iloc[0,2]
     date= df.iloc[0,0]
-    injection= df.iloc[0,4].replace(",","")
-    withdrawal=df.iloc[0,5].replace(",","")
-    trend= df.iloc[0,3].replace(",",".")
+    injection= df.iloc[0,4]
+    withdrawal=df.iloc[0,5]
+    trend= df.iloc[0,3]
     trend_icon= "📈" if float(trend)>0 else "📉"
-    injection_capacity= df.iloc[0,7].replace(",","")
-    withdrawal_capacity= df.iloc[0,8].replace(",","")
+    injection_capacity= df.iloc[0,7]
+    withdrawal_capacity= df.iloc[0,8]
     
     
     def progessbar(fuellstand):
@@ -43,9 +64,9 @@ def get_tweet_from_df(df):
         return bar
     
     tweet = f"Stand: \t{date} \n"+ \
-    f"Gas-Fuellstand: \t{progessbar(fuellstand)} {fuellstand}%\n" + \
-    f"Gas-Zufuhr: \t{injection} GWh von max. {injection_capacity} GWh\n" + \
-    f"Gas-Entnahme: \t{withdrawal} GWh von max. {withdrawal_capacity} GWh" + \
+    f"Gas-Fuellstand: \t{progessbar(fuellstand)} {fuellstand:.0f}%\n" + \
+    f"Gas-Zufuhr: \t{injection:.2f} GWh von max. {injection_capacity:.2f} GWh\n" + \
+    f"Gas-Entnahme: \t{withdrawal:.2f} GWh von max. {withdrawal_capacity:.2f} GWh" + \
     f"\nTrend: \t{trend}% {trend_icon}" 
     
     return tweet
@@ -54,8 +75,11 @@ def get_tweet_from_df(df):
     
 
 def main():
-    convert_to_xlsx()
-    df= pd.read_excel('Germnay.xlsx')
+    api_key = "ENTER_API_KEY_HERE"
+    start = datetime.date(2022,1,1).isoformat()
+    end = datetime.date(2022,3,31).isoformat()
+    
+    df = get_gas_data(api_key,start,end)
     tweet = get_tweet_from_df(df)
     print(tweet)
     return tweet
